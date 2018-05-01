a749b3cde702e948bab985364b1c2d71d3074a07
TRottinger <TRRottinger@gmail.com>
2018-04-30 22:50:04 -0400

    Fixed bad english

--- src/main/java/com/rottinford/main/Main.java
+++ src/main/java/com/rottinford/main/Main.java
@@ -1,6 +1,7 @@
-package com.TAndD;
+package main.java.com.rottinford.main;
 
 public class Main {
+
     public static void main(String[] args)
     {
         String start = "haHAA now you do all the work xD";
