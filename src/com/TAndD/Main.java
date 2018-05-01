357a51190090fa0c18361a8c12132616f1319829
Derek Ledford <lehquack@gmail.com>
2018-04-30 22:12:14 -0400

    Testing push

--- src/com/TAndD/Main.java
+++ src/com/TAndD/Main.java
@@ -3,7 +3,7 @@ package com.TAndD;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("xDDDDD");
-        System.out.println("Second Push");
+        String start = "haHAA now you do all the work xD";
+        System.out.print(start);
     }
 }
